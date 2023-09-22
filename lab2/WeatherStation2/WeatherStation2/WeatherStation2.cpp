1a36cb760f8162161019e4f3434be08933996a0b
Daniil <koldyrev98@icloud.com>
2023-09-22 17:22:10 +0300

    Add 2.2

---
+++ lab2/WeatherStation2/WeatherStation2/WeatherStation2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+#include "WeatherData.h"
+
+int main()
+{
+	CWeatherData wd;
+
+	CDisplay display;
+	wd.RegisterObserver(display);
+
+	CStatsDisplay statsDisplay;
+	wd.RegisterObserver(statsDisplay);
+	CStatsDisplay statsDisplay2;
+	wd.RegisterObserver(statsDisplay2);
+
+	wd.SetMeasurements(3, 0.7, 760);
+	wd.SetMeasurements(4, 0.8, 761);
+	wd.RemoveObserver(statsDisplay);
+
+	wd.SetMeasurements(10, 0.8, 761);
+	wd.SetMeasurements(-10, 0.8, 761);
+	return 0;
+}
+
---
+++ lab2/WeatherStation2/WeatherStation2_tests/WeatherStation2_tests.cpp
@@ -0,0 +1,55 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../../catch/catch.hpp"
+#include "../WeatherStation2/Observer.h"
+#include "../WeatherStation2/WeatherData.h"
+TEST_CASE("Test with Remove")
+{
+    CWeatherData wd;
+    CDisplay display;
+    CStatsDisplay statsDisplay;
+    CStatsDisplay statsDisplay2;
+
+    wd.RegisterObserver(display);
+    wd.RegisterObserver(statsDisplay);
+    wd.RegisterObserver(statsDisplay2);
+
+    SECTION("Remove observer during update") {
+        // Устанавливаем начальные значения
+        wd.SetMeasurements(3, 0.7, 760);
+        wd.SetMeasurements(4, 0.8, 761);
+
+        // Удаляем одного из наблюдателей во время обновления
+        wd.RemoveObserver(statsDisplay);
+
+        // Устанавливаем новые значения
+        wd.SetMeasurements(10, 0.8, 761);
+        wd.SetMeasurements(-10, 0.8, 761);
+
+        // В данном случае statsDisplay больше не должен получать обновления
+        REQUIRE(statsDisplay.GetMinTemperature() == 3.0);
+        REQUIRE(statsDisplay.GetMaxTemperature() == 10.0);
+        REQUIRE(statsDisplay.GetMinHumidity() == 0.7);
+        REQUIRE(statsDisplay.GetMaxHumidity() == 0.8);
+        REQUIRE(statsDisplay.GetMinPressure() == 760.0);
+        REQUIRE(statsDisplay.GetMaxPressure() == 761.0);
+    }
+
+    SECTION("No observer removal during update") {
+        // Устанавливаем начальные значения
+        wd.SetMeasurements(3, 0.7, 760);
+        wd.SetMeasurements(4, 0.8, 761);
+
+        // Устанавливаем новые значения без удаления наблюдателей
+        wd.SetMeasurements(10, 0.8, 761);
+        wd.SetMeasurements(-10, 0.8, 761);
+
+        // Все наблюдатели должны получить обновления
+        REQUIRE(statsDisplay.GetMinTemperature() == -10.0);
+        REQUIRE(statsDisplay.GetMaxTemperature() == 10.0);
+        REQUIRE(statsDisplay.GetMinHumidity() == 0.7);
+        REQUIRE(statsDisplay.GetMaxHumidity() == 0.8);
+        REQUIRE(statsDisplay.GetMinPressure() == 760.0);
+        REQUIRE(statsDisplay.GetMaxPressure() == 761.0);
+    }
+
+}
